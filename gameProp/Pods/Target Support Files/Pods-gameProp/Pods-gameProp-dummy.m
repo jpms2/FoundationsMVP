90927f26766833e43e43f22e8a88b0e926e6b9c3
João Pedro de Medeiros Santos <jpms2@172-16-3-33.0000.ufpe.br.iosda.org>
2018-05-07 15:31:39 -0300

    project with popups

---
+++ gameProp/Pods/Target Support Files/Pods-gameProp/Pods-gameProp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gameProp : NSObject
+@end
+@implementation PodsDummy_Pods_gameProp
+@end
---
+++ gameProp/gameProp/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  gameProp
+//
+//  Created by João Pedro de Medeiros Santos on 07/05/18.
+//  Copyright © 2018 João Pedro de Medeiros Santos. All rights reserved.
+//
+
+import UIKit
+import Toast_Swift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    func makeToast(_ text:String,_ posX:Int,_ posY:Int){
+        let pos = CGPoint(x: posX, y: posY)
+        var style = ToastStyle()
+        //style.activitySize = size
+        style.displayShadow = true
+        style.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        ToastManager.shared.style = style
+        self.view.makeToast(text,duration: 10.0, point: pos, title: "Dica!", image: nil, completion: { didTap in
+            if didTap {
+                print("completion from tap")
+            } else {
+                print("completion without tap")
+            }
+        })
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        makeToast("Comece com a poupança!", 100, 100)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
