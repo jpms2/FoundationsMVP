e736b383e6a1ad8b8c57a8c9296e2f15ca101ff0
João Pedro de Medeiros Santos <jpms2@172-16-3-21.0000.ufpe.br.iosda.org>
2018-05-11 19:59:38 -0300

    yeld and transactions working properly

---
+++ gameProp/gameProp/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  gameProp
+//
+//  Created by João Pedro de Medeiros Santos on 5/11/18.
+//  Copyright © 2018 João Pedro de Medeiros Santos. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
